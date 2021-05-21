9d4bf0b37f0a240a46fe2b26672176b8627fd8d5
Olivier Couvez <o.couvez@free.fr>
2021-05-21 09:10:53 +0200

    first commit

---
+++ DonneesNavigation/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Sockets;
+using System.Windows.Threading;
+using System.Text.RegularExpressions;
+
+namespace DonneesNavigation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private RecupNav recupdonnees;
+        DispatcherTimer timerRecup = new DispatcherTimer();
+        string lesDonnees = "";
+        string[] lesDonneesLignes = new string[];
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDemarre_Click(object sender, RoutedEventArgs e)
+        {
+            connexion();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            LectureCommande();
+        }
+
+        private void LectureCommande()
+        {
+            try
+            {
+                lesDonnees = recupdonnees.Lecture();
+
+                
+
+                lesDonneesLignes = Regex.Split(lesDonnees, "\n\\s*");
+            }
+            catch
+            {
+                timerRecup.Stop();
+                MessageBoxResult resultat;
+                lblconnexion.Content = "Connexion perdu !";
+                resultat = MessageBox.Show("Il semblerai que nous ayons perdu la connexion, réessayer ?", "Erreur connexion", MessageBoxButton.OKCancel, MessageBoxImage.Error);
+
+                if (resultat == MessageBoxResult.OK)
+                {
+                    connexion();
+                }
+                else
+                {
+                    btnQuitter.Focus();
+                }
+            }
+        }
+
+
+        private void connexion()
+        {
+            recupdonnees = new RecupNav("localhost", 3000);
+            try
+            {
+                timerRecup.IsEnabled = true;
+
+
+                timerRecup.Tick += timer_Tick;
+
+                timerRecup.Interval = TimeSpan.FromSeconds(0);
+                timerRecup.Start();
+                lblconnexion.Content = "Connexion Ok !";
+
+            }
+            catch
+            {
+                btnQuitter.Focus();
+            }
+        }
+
+        private void btnQuitter_Click(object sender, RoutedEventArgs e)
+        {
+            timerRecup.Stop();
+            this.Close();
+        }
+    }
+}
---
+++ DonneesNavigation/RecupNav.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows;
+
+namespace DonneesNavigation
+{
+    class RecupNav
+    {
+        private int Port;
+        private string AdrTCP;
+        private Byte[] Commande;
+        TcpClient SocketClient;
+        NetworkStream NetStream;
+
+        public RecupNav(string AdrTCP, int Port)
+        {
+
+            try
+            {
+                SocketClient = new TcpClient();
+                SocketClient.Connect(AdrTCP, Port);
+                NetStream = SocketClient.GetStream();
+            }
+
+            catch (ArgumentOutOfRangeException err)
+            {
+                MessageBox.Show("Une erreur est survenue : " + err.Message +
+                "\nImpossible de se connecter à l'hote, vérifiez l'adresse IP ? ");
+            }
+
+            catch (SocketException err)
+            {
+                MessageBox.Show("Une erreur est survenue : " + err.Message + 
+                "\nImpossible de se connecter à l'hote, vérifiez l'adresse IP ? ");
+            }          
+        }
+
+        public int Port1 { get => Port; set => Port = value; }
+        public string AdrTCP1 { get => AdrTCP; set => AdrTCP = value; }
+
+        public string Lecture()
+        {
+            string returndata = "";
+
+            if (NetStream.CanRead)
+            {
+                // Reads NetworkStream into a byte buffer.
+                byte[] bytes = new byte[SocketClient.ReceiveBufferSize];
+
+                // Read can return anything from 0 to numBytesToRead.
+                // This method blocks until at least one byte is read.
+                NetStream.Read(bytes, 0, (int)SocketClient.ReceiveBufferSize);
+
+                // Returns the data received from the host to the console.
+                returndata = Encoding.UTF8.GetString(bytes);
+
+                return returndata;
+            }
+            else
+            {
+
+                return returndata;
+            }
+        }
+    }
+}
